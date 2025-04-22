f53020257feee3d674dd50fe41a0a3cc0bac83d6
AgusPaez <agus.123.paez@gmail.com>
2025-04-22 19:38:48 -0300

    error: reload corregido

--- src/components/ExpresoCuyano/ExpresoCuyano.jsx
+++ src/components/ExpresoCuyano/ExpresoCuyano.jsx
@@ -8,17 +8,15 @@ import { Form } from "../Form";
 
 export const ExpresoCuyano = () => {
   const { user } = useAuth();
-  const [data, setData] = useState([]);
-  const FetchGetPromocion = async () => {
-    const response = await getPromocion();
-    setData(response.data);
-  };
+  const { getPromocion, deletePromocion, promociones } = useExpresoCuyano();
+  // const [data, setData] = useState([]);
+  // const FetchGetPromocion = async () => {
+  //   const response = await getPromocion();
+  //   setData(response.data);
+  // };
   useEffect(() => {
-    FetchGetPromocion();
+    getPromocion();
   }, [user]);
-
-  const { getPromocion, deletePromocion } = useExpresoCuyano();
-
   const HandleWspClick = (datos) => {
     const message = `Hola,necesito más información acerca de esta promoción de viaje:
   *Titulo:* ${datos.titulo}
@@ -39,15 +37,15 @@ export const ExpresoCuyano = () => {
         success: "Promocion eliminada exitosamente",
         error: "Error al eliminar la promocion",
       });
-
-      FetchGetPromocion();
+      getPromocion();
+      // FetchGetPromocion();
     } catch (error) {
       console.error("Error al eliminar la promocion", error);
     }
   }
 
   return (
-    <section id="expresocuyano" className="   ">
+    <section id="expresocuyano" className="mb-[4rem]">
       <div className="flex flex-col gap-20 p-4">
         <div>
           <h2 className="font-orbitron text-4xl text-center font-semibold text-blue-950">
@@ -149,66 +147,72 @@ export const ExpresoCuyano = () => {
         <div className="mx-auto">
           <h2 className="font-orbitron text-2xl text-center font-semibold text-blue-950 ">
             Promociones de viajes
-            <hr className="w-64 mx-auto mt-2 text-[#660000]" />
+            {/* <hr className="w-64 mx-auto mt-2 text-[#660000]" /> */}
           </h2>
         </div>
 
-        <div className=" flex flex-col justify-center items-center gap-4">
-          {data.map((promos) => (
-            <div
-              className="flex flex-col items-center justify-center min-w-[350px] h-[350px]  md:w-[750px] md:h-[200px] p-4 border border-gray-400 rounded-2xl transition-all duration-300 hover:border-gray-600 md:flex-row md:items-center md:justify-between "
-              key={promos.id}
-            >
-              <img
-                src="/images/cardexpress.png"
-                width={250}
-                height={50}
-                className="rounded-lg bg-cover bg-center w-[200px] "
-              />
-              <div className="flex flex-col justify-around gap-3 mt-1 md:w-[400px]">
-                <h2 className="text-blue-700 font-bold font-poppins">
-                  {promos.titulo}
-                </h2>
-                <p className="text-gray-600 font-poppins">
-                  {promos.descripcion}
-                </p>
-                <h3 className="text-green-500 font-semibold">
-                  $ ARS {promos.precio}
-                </h3>
-                <div className="mx-auto bg-[#2A4865] p-3 rounded-full scale-90 transition-all hover:scale-100 cursor-pointer duration-300">
-                  <a
-                    onClick={() => HandleWspClick(promos)}
-                    className="text-white font-semibold"
-                  >
-                    Reservar Ahora
-                  </a>
-                </div>
-              </div>
-
-              {!user ? (
-                ""
-              ) : (
-                <div className="flex  w-full justify-between md:flex md:flex-col md:justify-around md:gap-10 md:w-10">
-                  <button
-                    onClick={() => DeletePromoById(promos.id)}
-                    className="text-blue-600 hover:text-red-500 transition-all duration-300 cursor-pointer"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="1.5em"
-                      height="1.5em"
-                      viewBox="0 0 24 24"
+        <div className="flex flex-col justify-center items-center gap-4">
+          {promociones.length === 0 ? (
+            <p className="text-center text-gray-600 font-poppins">
+              No hay promociones disponibles.
+            </p>
+          ) : (
+            promociones.map((promos) => (
+              <div
+                className="flex flex-col items-center justify-center min-w-[350px] h-[350px] md:w-[750px] md:h-[200px] p-4 border border-gray-400 rounded-2xl transition-all duration-300 hover:border-gray-600 md:flex-row md:items-center md:justify-between"
+                key={promos.id}
+              >
+                <img
+                  src="/images/cardexpress.png"
+                  width={250}
+                  height={50}
+                  className="rounded-lg bg-cover bg-center w-[200px]"
+                />
+                <div className="flex flex-col justify-around gap-3 mt-1 md:w-[400px]">
+                  <h2 className="text-blue-700 font-bold font-poppins">
+                    {promos.titulo}
+                  </h2>
+                  <p className="text-gray-600 font-poppins">
+                    {promos.descripcion}
+                  </p>
+                  <h3 className="text-green-500 font-semibold">
+                    $ ARS {promos.precio}
+                  </h3>
+                  <div className="mx-auto bg-[#2A4865] p-3 rounded-full scale-90 transition-all hover:scale-100 cursor-pointer duration-300">
+                    <a
+                      onClick={() => HandleWspClick(promos)}
+                      className="text-white font-semibold"
                     >
-                      <path
-                        fill="currentColor"
-                        d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"
-                      />
-                    </svg>
-                  </button>
+                      Reservar Ahora
+                    </a>
+                  </div>
                 </div>
-              )}
-            </div>
-          ))}
+
+                {!user ? (
+                  ""
+                ) : (
+                  <div className="flex w-full justify-between md:flex md:flex-col md:justify-around md:gap-10 md:w-10">
+                    <button
+                      onClick={() => DeletePromoById(promos.id)}
+                      className="text-blue-600 hover:text-red-500 transition-all duration-300 cursor-pointer"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="1.5em"
+                        height="1.5em"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                )}
+              </div>
+            ))
+          )}
         </div>
       </div>
     </section>
