26b6a4d5cd955b83b6f501976798d4f187251d09
AgusPaez <agus.123.paez@gmail.com>
2025-04-22 19:38:10 -0300

    add validation

--- src/components/hooks/useFormHook.js
+++ src/components/hooks/useFormHook.js
@@ -1,52 +1,46 @@
-import { object } from 'framer-motion/client';
-import React, { useState } from 'react'
+import { object } from "framer-motion/client";
+import React, { useState } from "react";
 
 export const useFormHook = (initialValue = {}) => {
-    const [input, setInput] = useState(initialValue);
-    const [error, setError] = useState(initialValue);
-    const handleChange = ({ target }) => {
+  const [input, setInput] = useState(initialValue);
+  const [error, setError] = useState(initialValue);
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
 
-        const { name, value } = target;
-        setInput(prev => ({
-            ...prev,
-            [name]: value,
-        }));
+    setError((prev) => ({
+      ...prev,
+      [name]: "",
+    }));
+  };
 
-        setError(prev => ({
+  const validate = () => {
+    const newErrors = {};
 
+    if (!input.titulo) newErrors.titulo = "El titulo es obligatorio";
+    if (!input.descripcion)
+      newErrors.descripcion = "La descripcion es obligatoria";
+    if (!input.precio) newErrors.precio = "El precio es invalido";
+    if (!input.tipo) newErrors.tipo = "Selecciona el tipo de promoción";
+    // if (!input.valida_hasta) newErrors.valida_hasta = 'Fecha requerida'
 
-            ...prev,
-            [name]: ""
-        }))
+    setError(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
 
-
-
-
-    };
-
-    const validate = () => {
-        const newErrors = {}
-
-        if (!input.titulo) newErrors.titulo = 'El titulo es obligatorio'
-        if (!input.descripcion) newErrors.descripcion = 'La descripcion es obligatoria'
-        if (!input.precio) newErrors.precio = 'El precio es invalido'
-        if (!input.valida_hasta) newErrors.valida_hasta = 'Fecha requerida'
-
-        setError(newErrors)
-        return Object.keys(newErrors).length === 0
-    }
-
-    const reset = () => {
-        setInput(initialValue)
-        setError({})
-    }
-    return {
-        handleChange,
-        input,
-        setInput,
-        reset,
-        validate,
-        error
-    }
-
-}
+  const reset = () => {
+    setInput(initialValue);
+    setError({});
+  };
+  return {
+    handleChange,
+    input,
+    setInput,
+    reset,
+    validate,
+    error,
+  };
+};
